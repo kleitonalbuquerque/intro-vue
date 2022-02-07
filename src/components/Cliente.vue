81d31d669660bfdb873e3141bf7c5eef15c141ab
kleitonalbuquerque <kleiton2102@gmail.com>
2022-02-07 17:20:21 -0300

    First commit

---
+++ src/components/Cliente.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="cliente">
+    <h2 class="cliente-nome">Cliente aqui!</h2>
+    <h3>Descrição do cliente: Bla bla bla...</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#cliente {
+  color: rgb(0, 255, 0);
+  background-color: black;
+  max-width: 600px;
+  height: 150px;
+  margin: 20px;
+  padding: 10px;
+}
+
+.cliente-nome {
+  color: rgb(255, 255, 255);
+}
+</style>
