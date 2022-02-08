124b3bd008e51b74adbae346f5349b4918351a38
kleitonalbuquerque <kleiton2102@gmail.com>
2022-02-07 23:45:05 -0300

    V-for

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
 <template>
   <div id="app">
     <h1>Guia Clientes</h1>
-    <!-- <Cliente :cliente="clienteMariana" showIdade="true" /> -->
-    <!-- <Cliente
-      nome="Mariana"
-      email="mariana@testemail.com"
-      :idade="2"
-      :showIdade="true"
-    />
-    <Cliente
-      nome="Rafaella"
-      email="rafa@testemail.com"
-      :idade="18"
-      :showIdade="true"
-    />
-    <Cliente
-      nome="Luiz Paulo"
-      email="lp@testemail.com"
-      :idade="16"
-      :showIdade="false"
-    />
-    <Cliente
-      nome="Diana"
-      email="diana@testemail.com"
-      :idade="13"
-      :showIdade="true"
-    />
-    <Cliente
-      nome="Beatriz"
-      email="bia@testemail.com"
-      :idade="10"
-      :showIdade="true"
-    /> -->
-
     <div v-for="cliente in clientes" :key="cliente.id">
       <p>
         <Cliente :cliente="cliente" :showIdade="true" />
@@ -52,12 +20,6 @@ export default {
   name: "App",
   data() {
     return {
-      clienteMariana: {
-        id: 1,
-        nome: "Mariana",
-        email: "mariana@testemail.com",
-        idade: 2,
-      },
       clientes: [
         {
           id: 1,
